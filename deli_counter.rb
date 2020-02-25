52b3d196c95665e6c04b8fa6a218397d5b8485c3
Steven Ross <ross.steven@me.com>
2020-02-25 22:37:53 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
 
-    katz_deli.each do |customer, index|
+    katz_deli.each.with_index(1) do |customer, index|
       line_position << "#{index}. #{customer}"
 
     end
